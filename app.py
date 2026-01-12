1f3a25ea24dfedf98fd456ad123c93f786c6cf53
mrodriguezcartabia <mrodriguezcartabia@gmail.com>
2026-01-12 19:22:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -476,8 +476,25 @@ with herramientas:
 
     # Resultado del hallado
     variable = st.session_state.variable_optimizada if st.session_state.variable_optimizada else ""
-    valor = f"{st.session_state.resultado_opt:5f}" if st.session_state.resultado_opt else ""
+    valor = f"{st.session_state.resultado_opt:.5f}" if st.session_state.resultado_opt else ""
     st.metric(label=f"{variable}=", value=valor)
+    st.markdown(f"""
+        <div style="margin-bottom: 16px;">         
+            <div style="
+                height: 42px;
+                background-color: #1e293b; 
+                color: #fafafa; 
+                padding: 0px 12px; 
+                border: 1px solid rgba(255, 255, 255, 0.2); 
+                border-radius: 0.5rem;
+                display: flex;
+                align-items: center;
+                font-size: 1rem;
+                font-family: 'Inter', sans-serif;">
+                {variable} = {valor} 
+            </div>
+        </div>
+    """, unsafe_allow_html=True)
 
 
 # Calculamos los valores del call
