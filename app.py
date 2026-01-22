97a0b90db82d252e35fa45a229338e28ed4c8e05
mrodriguezcartabia <mrodriguezcartabia@gmail.com>
2026-01-22 12:15:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,8 +164,8 @@ def get_market_data_alpha():
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
         response = requests.get(f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AMZN&apikey={api_key}", headers=headers, timeout=10)
         data = response.json()
-        with st.expander(t["msg_error_api"]):
-            st.json(data)
+        #with st.expander(t["msg_error_api"]):
+        #    st.json(data)
         if "Global Quote" in data and "05. price" in data["Global Quote"]:
             precio = float(data["Global Quote"]["05. price"])
             
